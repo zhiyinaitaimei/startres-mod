07227eaa3b40e89d045416916f73a8308c66ece8
zhiyinaitaimei <2889623622@qq.com>
2025-07-19 18:42:35 +0800

    Update IncarnationConfig.java

--- src/main/java/com/example/startres/IncarnationConfig.java
+++ src/main/java/com/example/startres/IncarnationConfig.java
@@ -52,7 +52,7 @@ public class IncarnationConfig {
                                 .translation("config.startres.incarnation.wander_range")
                                 .defineInRange("wander_range", 8.0, 1.0, 50.0);
 
-                MODE_SWITCH_ENABLED = builder.comment("是否允许化身切换模式（Shift+右键）实验功能!可能导致坏档！")
+                MODE_SWITCH_ENABLED = builder.comment("允许Shift+右键切换化身模式（切换Boss化身可能导致坏档!）")
                                 .translation("config.startres.incarnation.mode_switch_enabled")
                                 .define("mode_switch_enabled", false);
 
